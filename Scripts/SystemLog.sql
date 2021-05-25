8ab597481612e7bf9b4468c43afc6c2edd578585
Edgy190 <66889568+Edgy190@users.noreply.github.com>
2021-05-25 14:32:09 -0600

    Add files via upload

---
+++ Scripts/SystemLog.sql
@@ -0,0 +1,27 @@
+/*Autor: Renzo Barra, 5/25/2021*/
+CREATE TABLE PE.SYSTEMLOG (
+	SYSTEMLOG_ID INT COMMENT 'SystemLog identification code.', 
+	DESCRIPTION VARCHAR(150) COMMENT 'SystemLog description.', 
+	OBJECT VARCHAR(50) COMMENT 'SystemLog reference object.', 
+	TYPE_CHANGE VARCHAR(50) COMMENT 'SystemLog type of change.', 
+	SYS_DATE DATETIME, 
+	CREATION_USER VARCHAR(50)
+    );
+
+ALTER TABLE PE.SYSTEMLOG  COMMENT 'Repository to storage information about systemLog.';
+
+ALTER TABLE PE.SYSTEMLOG
+AUTO_INCREMENT = 0,
+CHANGE COLUMN SYSTEMLOG_ID SYSTEMLOG_ID INT NOT NULL AUTO_INCREMENT COMMENT 'SystemLog identification code.',
+ADD PRIMARY KEY (SYSTEMLOG_ID);
+
+/*======================================TRIGGERS======================================*/
+/*INSERT TRIGGER*/
+DROP TRIGGER IF EXISTS PE.SYSTEMLOG_BEFORE_INSERT;
+DELIMITER $$
+USE PE$$
+CREATE DEFINER = CURRENT_USER TRIGGER PE.SYSTEMLOG_BEFORE_INSERT BEFORE INSERT ON SYSTEMLOG FOR EACH ROW
+	BEGIN
+		Set new.sys_date = SYSDATE();
+		Set new.creation_user = USER();
+END$$
